ab202a9725b14cf0256a54c904852e43ed6cd498
Jordy Yeoman <jordyyeoman@gmail.com>
2022-09-17 23:15:03 +0800

    Query data from specific collections

--- amara-blackwell-art/src/components/Hero.tsx
+++ amara-blackwell-art/src/components/Hero.tsx
@@ -1,20 +1,49 @@
-import React from "react";
-import IntersectionObserverWrapper from "./utils/IntersectionObserverWrapper";
+import { useStaticQuery, graphql } from 'gatsby';
+import React from 'react';
+import IntersectionObserverWrapper from './utils/IntersectionObserverWrapper';
+import { GatsbyImage } from 'gatsby-plugin-image';
 
 function Hero() {
+  const data = useStaticQuery(graphql`
+    query FeatureProductsQuery {
+      allShopifyProduct(limit: 10, filter: { tags: { in: "feature" } }) {
+        nodes {
+          description
+          status
+          priceRangeV2 {
+            maxVariantPrice {
+              amount
+            }
+          }
+          featuredImage {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  `);
+
   return (
-    <div className="h-[70vh] w-full flex">
-      <div className="h-full w-1/2 bg-red-200"></div>
-      <div className="h-full w-1/2 bg-green-200 flex flex-col justify-center">
-        <div className="text-black pl-16">
-          <IntersectionObserverWrapper duration={"duration-500"}>
-            <div className="text-6xl font-bold">Latest Pieces</div>
-            <div className="text-xl font-normal pt-2 max-w-lg">
+    <div className='h-[70vh] w-full flex'>
+      <div className='h-full w-1/2 bg-red-200'>
+        <GatsbyImage
+          className='object-fit object-cover h-full w-full'
+          image={
+            data?.allShopifyProduct?.nodes[2]?.featuredImage?.gatsbyImageData
+          }
+          alt={'Feature dog portrait artwork'}
+        />
+      </div>
+      <div className='h-full w-1/2 bg-green-200 flex flex-col justify-center'>
+        <div className='text-black pl-16'>
+          <IntersectionObserverWrapper duration={'duration-500'}>
+            <div className='text-6xl font-bold'>Latest Pieces</div>
+            <div className='text-xl font-normal pt-2 max-w-lg'>
               If you are going to use a passage of Lorem Ipsum, you need to be
               sure there isn't anything embarrassing hidden in the middle of
               text.
             </div>
-            <div className="cursor-pointer hover:scale-95 transition duration-300 text-xl font-normal px-8 py-2 mt-6 bg-yellow-200 w-fit rounded">
+            <div className='cursor-pointer hover:scale-95 transition duration-300 text-xl font-normal px-8 py-2 mt-6 bg-yellow-200 w-fit rounded'>
               Find out more
             </div>
           </IntersectionObserverWrapper>
--- amara-blackwell-art/src/components/SingleFeatureProduct.tsx
+++ amara-blackwell-art/src/components/SingleFeatureProduct.tsx
@@ -1,73 +1,76 @@
-import React from "react";
+import React from 'react';
+import IntersectionObserverWrapper from './utils/IntersectionObserverWrapper';
 
 function SingleFeatureProduct() {
   return (
     <>
-      <div className="min-w-screen min-h-screen bg-yellow-300 flex items-center p-5 lg:p-10 overflow-hidden relative">
-        <div className="w-full max-w-6xl rounded bg-white shadow-xl p-10 lg:p-20 mx-auto text-gray-800 relative md:text-left">
-          <div className="md:flex items-center -mx-10">
-            <div className="w-full md:w-1/2 px-10 mb-10 md:mb-0">
-              <div className="relative">
-                <img
-                  // Temporary
-                  src="https://i.etsystatic.com/22785252/r/il/bc7b4c/3370790004/il_570xN.3370790004_keov.jpg"
-                  className="w-full relative z-10"
-                  alt=""
-                />
-                <div className="border-4 border-yellow-200 absolute top-10 bottom-10 left-10 right-10 z-0"></div>
+      <div className='min-w-screen min-h-screen bg-yellow-300 flex items-center pt-96 p-5 lg:p-10 overflow-hidden relative'>
+        <IntersectionObserverWrapper duration={'duration-1000'}>
+          <div className='w-full max-w-6xl rounded bg-white shadow-xl p-10 lg:p-20 mx-auto text-gray-800 relative md:text-left'>
+            <div className='md:flex items-center -mx-10'>
+              <div className='w-full md:w-1/2 px-10 mb-10 md:mb-0'>
+                <div className='relative'>
+                  <img
+                    // Temporary
+                    src='https://i.etsystatic.com/22785252/r/il/bc7b4c/3370790004/il_570xN.3370790004_keov.jpg'
+                    className='w-full relative z-10'
+                    alt=''
+                  />
+                  <div className='border-4 border-yellow-200 absolute top-10 bottom-10 left-10 right-10 z-0'></div>
+                </div>
               </div>
-            </div>
-            <div className="w-full md:w-1/2 px-10">
-              <div className="mb-10">
-                <h1 className="font-bold uppercase text-2xl mb-5">
-                  Bestfriend <br />
-                  Dog Portraits
-                </h1>
-                <p className="text-sm">
-                  Nothing better to appreciate your pet with than a portrait.
-                  Enjoy seeing your best friend preserved through time on a
-                  canvas, oil painting, drawing or even digital art!
-                  {/* <a
+              <div className='w-full md:w-1/2 px-10'>
+                <div className='mb-10'>
+                  <h1 className='font-bold uppercase text-2xl mb-5'>
+                    Bestfriend <br />
+                    Dog Portraits
+                  </h1>
+                  <p className='text-sm'>
+                    Nothing better to appreciate your pet with than a portrait.
+                    Enjoy seeing your best friend preserved through time on a
+                    canvas, oil painting, drawing or even digital art!
+                    {/* <a
                     href="#"
                     className="opacity-50 text-gray-900 hover:opacity-100 inline-block text-xs leading-none border-b border-gray-900"
                   >
                     MORE <i className="mdi mdi-arrow-right"></i>
                   </a> */}
-                </p>
-              </div>
-              <div>
-                <div className="inline-block align-bottom mr-5">
-                  <span className="text-2xl leading-none align-baseline">
-                    $
-                  </span>
-                  <span className="font-bold text-5xl leading-none align-baseline">
-                    800
-                  </span>
-                  {/* <span className="text-2xl leading-none align-baseline">
+                  </p>
+                </div>
+                <div>
+                  <div className='inline-block align-bottom mr-5'>
+                    <span className='text-2xl leading-none align-baseline'>
+                      $
+                    </span>
+                    <span className='font-bold text-5xl leading-none align-baseline'>
+                      800
+                    </span>
+                    {/* <span className="text-2xl leading-none align-baseline">
                     .99
                   </span> */}
-                </div>
-                <div className="inline-block align-bottom">
-                  <button className="bg-yellow-300 opacity-75 hover:opacity-100 text-yellow-900 hover:text-gray-900 rounded-full px-10 py-2 font-semibold">
-                    <i className="mdi mdi-cart -ml-2 mr-2"></i> BUY NOW
-                  </button>
+                  </div>
+                  <div className='inline-block align-bottom'>
+                    <button className='bg-yellow-300 opacity-75 hover:opacity-100 text-yellow-900 hover:text-gray-900 rounded-full px-10 py-2 font-semibold'>
+                      <i className='mdi mdi-cart -ml-2 mr-2'></i> BUY NOW
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </IntersectionObserverWrapper>
       </div>
-      <div className="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
+      <div className='flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10'>
         <div>
           <a
-            title="Buy me a beer"
-            href="https://www.buymeacoffee.com/jordyyeoman"
-            target="_blank"
-            className="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12"
+            title='Buy me a beer'
+            href='https://www.buymeacoffee.com/jordyyeoman'
+            target='_blank'
+            className='block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12'
           >
             <img
-              className="object-cover object-center w-full h-full rounded-full"
-              src="https://i.pinimg.com/originals/60/fd/e8/60fde811b6be57094e0abc69d9c2622a.jpg"
+              className='object-cover object-center w-full h-full rounded-full'
+              src='https://i.pinimg.com/originals/60/fd/e8/60fde811b6be57094e0abc69d9c2622a.jpg'
             />
           </a>
         </div>
