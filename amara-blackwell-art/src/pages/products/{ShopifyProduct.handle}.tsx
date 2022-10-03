93222c2fd6fc0b348d766c78f54ba33adf67aa13
Jordy Yeoman <jordyyeoman@gmail.com>
2022-10-03 15:42:34 +0800

    Add mobile design for product page

--- amara-blackwell-art/src/pages/products/{ShopifyProduct.handle}.tsx
+++ amara-blackwell-art/src/pages/products/{ShopifyProduct.handle}.tsx
@@ -8,77 +8,112 @@ import {
   MinusIcon,
 } from '@heroicons/react/24/solid';
 import Layout from '../../components/Layout';
+import { useState } from 'react';
+import useIntersectionObserver from '../../utils/hooks/useIntersectionObserver';
 
 const SingleProductPage = ({ data }) => {
+  const [quantity, setQuantity] = useState<number>(1);
   const { shopifyProduct } = data;
   const productImage = shopifyProduct.featuredImage.gatsbyImageData;
+  // Add To Cart helpers
+  const ref = React.useRef<HTMLDivElement | null>(null);
+  const entry = useIntersectionObserver(ref, {});
+  const isVisible = !!entry?.isIntersecting;
+
+  console.log('You visible?', isVisible);
 
   return (
     <Layout>
-      <div className='single-product bg-gray-200 py-8 px-12'>
-        <BreadCrumbs shopifyProduct={shopifyProduct} />
-        <div className='grid lg:grid-cols-2 w-full mt-4'>
-          <div className='col-span-1 flex justify-center'>
-            <div className='w-full h-full relative bg-red-200'>
-              {/* <GatsbyImage
+      <div className='single-product bg-slate-50 py-8 px-0 md:px-12'>
+        <div className='px-4 md:px-0'>
+          <BreadCrumbs shopifyProduct={shopifyProduct} />
+        </div>
+        <div className='grid grid-cols-1 lg:grid-cols-2 w-full mt-4'>
+          <div className='col-span-1 flex justify-center flex-col'>
+            <div className='block md:hidden px-4'>
+              <ProductHeading shopifyProduct={shopifyProduct} />
+            </div>
+            <div className='w-full h-full h-[calc(100vh-2rem)] relative bg-red-200'>
+              <GatsbyImage
                 className='object-fit object-cover h-full w-full'
                 image={productImage}
-                alt={productImage}
-              /> */}
+                alt={'TestProductImageTODOREMOVETODO'}
+              />
             </div>
           </div>
           <div className='col-span-1 flex justify-center'>
-            <div className='product-info bg-purple-400 rounded-md shadow-xl mx-16 px-16 py-8'>
-              <h1 className='single-product-title text-white text-2xl lg:text-3xl font-bold drop-shadow-xl'>
-                {shopifyProduct.title}
-              </h1>
-              <div className='grid lg:grid-cols-2 flex flex-row'>
-                <div className='col-span-1'>
-                  <p className='single-product-description mt-2 font-secondary-font font-bold text-white text-lg'>
-                    $399.00
-                  </p>
-                </div>
-                <div className='col-span-1 flex flex-row items-center'>
-                  <p className='single-product-description mt-2 font-secondary-font font-bold text-white text-lg'>
-                    In Stock
-                  </p>
-                  <CheckCircleIcon className='ml-2 mt-1 text-green-300 w-6 h-6' />
-                </div>
+            <div className='w-full px-4 py-8'>
+              <div className='hidden md:block'>
+                <ProductHeading shopifyProduct={shopifyProduct} />
               </div>
-              <p className='single-product-description mt-4 text-white text-md'>
+              <p className='single-product-description text-white text-md'>
                 {shopifyProduct.description}
               </p>
-              <div className='stars flex flex-row mt-2'>
-                <StarIcon className='h-8 w-8 text-yellow-400 m-1' />
-                <StarIcon className='h-8 w-8 text-yellow-400 m-1' />
-                <StarIcon className='h-8 w-8 text-yellow-400 m-1' />
-                <StarIcon className='h-8 w-8 text-yellow-400 m-1' />
-                <StarIcon className='h-8 w-8 text-yellow-400 m-1' />
-              </div>
-              <div className='qty-btn-holder flex items-center flex-row mt-2'>
-                <button className='bg-black p-2 rounded-lg'>
-                  <MinusIcon className='text-white h-4 w-4' />
-                </button>
-                <input
-                  type='text'
-                  value='1'
-                  className='mx-2 bg-white text-purple-400 font-bold text-xl w-12 h-8 text-center rounded-lg'
-                />
-                <button className='bg-black p-2 rounded-lg'>
-                  <PlusIcon className='text-white h-4 w-4' />
-                </button>
-              </div>
-              <div className='buy-btn-holder flex justify-start text-left mt-5'>
-                <Link to='/'>
-                  <button className='bg-black px-8 py-2 text-white rounded-lg text-center transition-all duration-300 hover:bg-primary-gray'>
-                    Add to Cart
+              <div className='flex justify-between'>
+                <div className='stars flex flex-col'>
+                  <p className='text-sm'>Rating:</p>
+                  <div className='flex'>
+                    <StarIcon className='h-5 w-5 text-yellow-400' />
+                    <StarIcon className='h-5 w-5 text-yellow-400' />
+                    <StarIcon className='h-5 w-5 text-yellow-400' />
+                    <StarIcon className='h-5 w-5 text-yellow-400' />
+                    <StarIcon className='h-5 w-5 text-yellow-400' />
+                  </div>
+                </div>
+                <div className='qty-btn-holder flex items-center flex-row mt-2'>
+                  <button
+                    className='bg-black p-2 rounded-lg'
+                    onClick={() => {
+                      let newVal = quantity - 1;
+                      if (newVal < 1 || newVal === 1) {
+                        setQuantity(1);
+                        return;
+                      }
+                      setQuantity(newVal);
+                    }}
+                  >
+                    <MinusIcon className='text-white h-4 w-4' />
                   </button>
-                </Link>
-                <Link to='/'>
-                  <button className='ml-3 bg-white px-8 py-2 text-black rounded-lg text-center transition-all duration-300 hover:bg-primary-gray'>
-                    Buy Now
+                  <input
+                    type='number'
+                    id='qty'
+                    value={quantity}
+                    onChange={(inputVal) => {
+                      let val = parseInt(inputVal?.target?.value) ?? '';
+                      setQuantity(val);
+                    }}
+                    className='mx-2 text-slate-400 font-bold text-xl w-12 h-8 text-center rounded-lg'
+                  />
+                  <button className='bg-black p-2 rounded-lg'>
+                    <PlusIcon
+                      className='text-white h-4 w-4'
+                      onClick={() => {
+                        setQuantity(quantity + 1);
+                      }}
+                    />
                   </button>
-                </Link>
+                </div>
+              </div>
+              <div className='buy-btn-holder flex justify-start text-left mt-5 transition duration-300'>
+                <div
+                  id='addToCart'
+                  className={`fixed bottom-0 w-screen left-0 transition duration-300 ${
+                    isVisible ? 'relative' : ''
+                  }`}
+                >
+                  <Link to='/'>
+                    <button className='transition duration-300 bg-slate-900 px-8 py-3 text-white w-full text-center transition-all duration-300 hover:bg-primary-gray'>
+                      Add to Cart
+                    </button>
+                  </Link>
+                </div>
+                <div ref={ref}>
+                  <Link to='/'>
+                    <button className='w-full ml-3 bg-white px-8 py-2 text-black rounded-lg text-center transition-all duration-300 hover:bg-primary-gray'>
+                      Buy Now
+                    </button>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
@@ -88,24 +123,46 @@ const SingleProductPage = ({ data }) => {
   );
 };
 
+const ProductHeading = ({ shopifyProduct }) => {
+  return (
+    <div className='grid lg:grid-cols-2 flex flex-row pb-8'>
+      <h2 className='single-product-title text-slate-700 text-3xl lg:text-3xl font-bold drop-shadow-xl'>
+        {shopifyProduct.title}
+      </h2>
+      <p>{shopifyProduct?.category ?? 'Handbags'}</p>
+      <div className='col-span-1 flex flex-row items-center justify-between'>
+        <p className='single-product-description font-secondary-font font-bold text-slate-700 text-lg'>
+          ${shopifyProduct?.priceRangeV2?.maxVariantPrice?.amount}.00
+        </p>
+        <div className='flex items-center rounded bg-slate-300 px-2 py-1'>
+          <p className='single-product-description font-secondary-font font-bold text-slate-700 text-xs'>
+            In Stock
+          </p>
+          <CheckCircleIcon className='ml-2 fill-white w-4 h-4' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const BreadCrumbs = ({ shopifyProduct }) => {
   return (
-    <div className='flex items-center lg:items-start flex-col w-full'>
+    <div className='w-full items-start flex flex-col w-full'>
       <p className='text-[10px]'>
         <Link to='/'>
-          <span className='bg-white rounded px-2 py-1 transition duration-300 hover:bg-slate-300 cursor-pointer'>
+          <span className='bg-slate-200 rounded px-2 py-1 transition duration-300 hover:bg-slate-300 cursor-pointer'>
             Shop
           </span>
         </Link>
         <span className='rounded py-1'>{' > '}</span>
         <Link to='/'>
-          <span className='bg-white rounded px-2 py-1 transition duration-300 hover:bg-slate-300 cursor-pointer'>
+          <span className='bg-slate-200 rounded px-2 py-1 transition duration-300 hover:bg-slate-300 cursor-pointer'>
             Collections
           </span>
         </Link>
         <span className='rounded py-1'>{' > '}</span>
         <Link to='/'>
-          <span className='bg-white rounded px-2 py-1 transition duration-300 hover:bg-slate-300 cursor-pointer'>{` ${shopifyProduct.title}`}</span>
+          <span className='bg-slate-200 rounded px-2 py-1 transition duration-300 hover:bg-slate-300 cursor-pointer'>{` ${shopifyProduct.title}`}</span>
         </Link>
       </p>
     </div>
