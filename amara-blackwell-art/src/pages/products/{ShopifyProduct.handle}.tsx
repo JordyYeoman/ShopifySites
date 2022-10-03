7afd33591f199c752a3312ee4d5378a8ac5d1ad5
Jordy Yeoman <jordyyeoman@gmail.com>
2022-10-03 20:56:37 +0800

    Add responsiveness to product image page

--- amara-blackwell-art/src/pages/products/{ShopifyProduct.handle}.tsx
+++ amara-blackwell-art/src/pages/products/{ShopifyProduct.handle}.tsx
@@ -17,23 +17,24 @@ const SingleProductPage = ({ data }) => {
   const productImage = shopifyProduct.featuredImage.gatsbyImageData;
   // Add To Cart helpers
   const ref = React.useRef<HTMLDivElement | null>(null);
-  const entry = useIntersectionObserver(ref, {});
+  const entry = useIntersectionObserver(ref, {
+    threshold: 1,
+    rootMargin: '25%',
+  });
   const isVisible = !!entry?.isIntersecting;
-
-  console.log('You visible?', isVisible);
 
   return (
     <Layout>
-      <div className='single-product bg-slate-50 py-8 px-0 md:px-12'>
-        <div className='px-4 md:px-0'>
+      <div className='single-product bg-white relative z-10 px-0 md:px-12 md:pb-12 max-w-screen-2xl mx-auto xl:min-h-[90vh]'>
+        <div className='px-4 md:px-0 pt-8 md:pb-8'>
           <BreadCrumbs shopifyProduct={shopifyProduct} />
         </div>
-        <div className='grid grid-cols-1 lg:grid-cols-2 w-full mt-4'>
+        <div className='grid grid-cols-1 lg:grid-cols-2 w-full'>
           <div className='col-span-1 flex justify-center flex-col'>
             <div className='block md:hidden px-4'>
               <ProductHeading shopifyProduct={shopifyProduct} />
             </div>
-            <div className='w-full h-full h-[calc(100vh-2rem)] relative bg-red-200'>
+            <div className='w-full h-full h-[calc(100vh-2rem)] md:h-[calc(100vh-10rem)] xl:h-[calc(100vh-16rem)] max-h-screen-lg relative bg-red-200'>
               <GatsbyImage
                 className='object-fit object-cover h-full w-full'
                 image={productImage}
@@ -42,16 +43,19 @@ const SingleProductPage = ({ data }) => {
             </div>
           </div>
           <div className='col-span-1 flex justify-center'>
-            <div className='w-full px-4 py-8'>
+            <div className='w-full px-4 py-8 lg:pt-2 lg:px-6'>
               <div className='hidden md:block'>
                 <ProductHeading shopifyProduct={shopifyProduct} />
               </div>
-              <p className='single-product-description text-white text-md'>
-                {shopifyProduct.description}
-              </p>
-              <div className='flex justify-between'>
+              <div className='flex flex-col pb-2 lg:py-4'>
+                <p className='text-sm font-bold pb-1'>Description:</p>
+                <p className='single-product-description text-md'>
+                  {shopifyProduct.description}
+                </p>
+              </div>
+              <div className='flex justify-between py-4 lg:py-8'>
                 <div className='stars flex flex-col'>
-                  <p className='text-sm'>Rating:</p>
+                  <p className='text-sm font-bold pb-1'>Product Rating:</p>
                   <div className='flex'>
                     <StarIcon className='h-5 w-5 text-yellow-400' />
                     <StarIcon className='h-5 w-5 text-yellow-400' />
@@ -60,45 +64,48 @@ const SingleProductPage = ({ data }) => {
                     <StarIcon className='h-5 w-5 text-yellow-400' />
                   </div>
                 </div>
-                <div className='qty-btn-holder flex items-center flex-row mt-2'>
-                  <button
-                    className='bg-black p-2 rounded-lg'
-                    onClick={() => {
-                      let newVal = quantity - 1;
-                      if (newVal < 1 || newVal === 1) {
-                        setQuantity(1);
-                        return;
-                      }
-                      setQuantity(newVal);
-                    }}
-                  >
-                    <MinusIcon className='text-white h-4 w-4' />
-                  </button>
-                  <input
-                    type='number'
-                    id='qty'
-                    value={quantity}
-                    onChange={(inputVal) => {
-                      let val = parseInt(inputVal?.target?.value) ?? '';
-                      setQuantity(val);
-                    }}
-                    className='mx-2 text-slate-400 font-bold text-xl w-12 h-8 text-center rounded-lg'
-                  />
-                  <button className='bg-black p-2 rounded-lg'>
-                    <PlusIcon
-                      className='text-white h-4 w-4'
+                <div className='items-start justify-start flex-col'>
+                  <p className='text-sm font-bold pb-1'>Qty:</p>
+                  <div className='flex flex-row items-center'>
+                    <button
+                      className='bg-black p-2 rounded-lg'
                       onClick={() => {
-                        setQuantity(quantity + 1);
+                        let newVal = quantity - 1;
+                        if (newVal < 1 || newVal === 1) {
+                          setQuantity(1);
+                          return;
+                        }
+                        setQuantity(newVal);
                       }}
+                    >
+                      <MinusIcon className='text-white h-4 w-4' />
+                    </button>
+                    <input
+                      type='number'
+                      id='qty'
+                      value={quantity}
+                      onChange={(inputVal) => {
+                        let val = parseInt(inputVal?.target?.value) ?? '';
+                        setQuantity(val);
+                      }}
+                      className='mx-2 bg-slate-300 text-slate-700 font-bold text-xl w-12 h-8 text-center rounded-lg'
                     />
-                  </button>
+                    <button className='bg-black p-2 rounded-lg'>
+                      <PlusIcon
+                        className='text-white h-4 w-4'
+                        onClick={() => {
+                          setQuantity(quantity + 1);
+                        }}
+                      />
+                    </button>
+                  </div>
                 </div>
               </div>
-              <div className='buy-btn-holder flex justify-start text-left mt-5 transition duration-300'>
+              <div className='flex justify-start flex-col mt-5 transition duration-300'>
                 <div
                   id='addToCart'
-                  className={`fixed bottom-0 w-screen left-0 transition duration-300 ${
-                    isVisible ? 'relative' : ''
+                  className={`fixed transition-opacity w-screen left-0 bottom-0 duration-500 ${
+                    isVisible ? 'opacity-0 h-0' : 'h-auto opacity-1'
                   }`}
                 >
                   <Link to='/'>
@@ -107,9 +114,21 @@ const SingleProductPage = ({ data }) => {
                     </button>
                   </Link>
                 </div>
+                <div
+                  id='addToCart'
+                  className={`relative transition duration-300 ${
+                    isVisible ? 'relative w-full' : 'opacity-0 w-0'
+                  }`}
+                >
+                  <Link to='/'>
+                    <button className='transition duration-300 bg-slate-900 px-8 py-3 text-white w-full text-center transition-all duration-300 hover:bg-slate-400'>
+                      Add to Cart
+                    </button>
+                  </Link>
+                </div>
                 <div ref={ref}>
                   <Link to='/'>
-                    <button className='w-full ml-3 bg-white px-8 py-2 text-black rounded-lg text-center transition-all duration-300 hover:bg-primary-gray'>
+                    <button className='transition mt-2 duration-300 bg-slate-300 px-8 py-3 text-slate-700 w-full text-center transition-all duration-300 hover:bg-slate-400'>
                       Buy Now
                     </button>
                   </Link>
@@ -125,7 +144,7 @@ const SingleProductPage = ({ data }) => {
 
 const ProductHeading = ({ shopifyProduct }) => {
   return (
-    <div className='grid lg:grid-cols-2 flex flex-row pb-8'>
+    <div className='grid flex flex-row pt-4 lg:pt-0 pb-8'>
       <h2 className='single-product-title text-slate-700 text-3xl lg:text-3xl font-bold drop-shadow-xl'>
         {shopifyProduct.title}
       </h2>
@@ -176,6 +195,7 @@ export const SingleProductQuery = graphql`
       title
       handle
       vendor
+      description
       variants {
         availableForSale
         barcode
